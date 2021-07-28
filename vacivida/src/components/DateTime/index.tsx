80731c48da676d5d5b9a428efc5b1d95179da224
Airton Oliveira <ton020500@gmail.com>
2021-07-27 22:51:14 -0300

    add date picker

--- vacivida/src/components/DateTime/index.tsx
+++ vacivida/src/components/DateTime/index.tsx
@@ -1,24 +1,28 @@
-import "react-native-gesture-handler"
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Button } from "react-native-paper";
 
 
-export default function DataTempo () {
-    const date = new Date();
-    const dia = date.getDate();
-    const mes = date.getMonth();
-    const ano = date.getUTCFullYear();
-    let diastr = dia.toString();
-    let messtr = mes.toString();
-
-    if (dia < 10)
-        diastr = "0" + diastr;
-    
-    if (mes < 10)
-        messtr = "0" + messtr;
-    return(
-        <View>
-            <Text>{diastr}/{messtr}/{ano}</Text>
-        </View>
-    );
+export default function  DateTime() {
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+  return (
+    <View>
+      <View>
+        <Button children="" color="#909090" onPress={() => {setShow(true)}}/>
+        <Text style={{position: 'absolute'}}>{date.getDate()}/{date.getMonth()}/{date.getUTCFullYear()}</Text>
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode="date"
+          is24Hour={true}
+          display="default"
+          onChange = {(event, selectedDate) => { const currentDate = selectedDate || date; setDate(currentDate); setShow(false)}}
+        />
+      )}
+    </View>
+  );
 }
